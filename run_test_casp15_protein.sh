8f81faa904813852117647fe50a0e13ba7e03276
ryanyxli <lyxnb2333@gmail.com>
2024-12-30 11:05:02 +0800

    feat - update casp15 protein script

--- run_test_casp15_protein.sh
+++ run_test_casp15_protein.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
-PYTHON_BIN="python"
+export XLA_FLAGS="--xla_disable_hlo_passes=custom-kernel-fusion-rewriter --xla_gpu_enable_triton_gemm=false"
+#export XLA_FLAGS="--xla_disable_hlo_passes=custom-kernel-fusion-rewriter" 
+export XLA_PYTHON_CLIENT_PREALLOCATE=true
+export XLA_CLIENT_MEM_FRACTION=0.95
+export JAX_TRACEBACK_FILTERING=off
+PYTHON_BIN="/usr/local/envs/af3/bin/python"
 
 $PYTHON_BIN run_alphafold.py \
-    --json_path casp15_protein_msa/7qih_a_b_data.json \
+    --json_path casp15_protein_msa/7qih_a_b_data_seed1.json \
     --output_dir output_casp15_protein \
+    --model_dir /content/ \
+    --flash_attention_implementation=xla \
     --norun_data_pipeline
