e4a110cf6be93f5b6c9b812aeff52311af1e6301
ryanyxli <lyxnb2333@gmail.com>
2025-03-13 17:15:30 +0800

    dbg

--- alphafold3/model/diffusion/diffusion_transformer.py
+++ alphafold3/model/diffusion/diffusion_transformer.py
@@ -159,17 +159,19 @@ def self_attention(
   
   alpha0 = jnp.einsum('...qhc,...khc->...hqk', q * key_dim ** (-0.5), k)
   if foo.enable_vis:
+    bar = alpha0 * 0 + bias
     jax.debug.print("mask shape {} {}", mask.shape, mask.dtype)
     pair_mask = mask[None] * mask[:, None]
     jax.debug.print("alpha0 shape {} {}", alpha0.shape, alpha0.dtype)
     alpha0 = alpha0 * pair_mask.astype(alpha0)
     bias0 = bias * pair_mask.astype(bias)
     print_tensor("Diff_Atten_alpha0", alpha0)
-    print_tensor("DIff_Atten_b", bias0)
+    print_tensor("DIff_Atten_bias0", bias0)
     # print_tensor("DIff_Atten_b_fp32", bias0.astype(jnp.float32))
-    print_tensor("DIff_Atten_b", bias)
+    print_tensor("DIff_Atten_bias", bias)
+    print_tensor("DIFF_Atten_bar", bar)
     foo.enable_vis = False
-  logits = alpha0 + bias
+  logits = alpha0 + bias + bar * 0
   # logits = jnp.einsum('...qhc,...khc->...hqk', q * key_dim ** (-0.5), k) + bias
 
   if pair_logits is not None:
